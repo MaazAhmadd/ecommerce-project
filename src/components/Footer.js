018ef2f2abd5e4f20cda017ef0aab963841d0dd6
Maaz <maaz.ahmad21399@gmail.com>
2021-10-06 18:49:22 +0500

    sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <Wrapper>
       <h5>
         &copy; {new Date().getFullYear()}
-        <span>ComfySloth</span>
+        <span> ComfySloth </span>
       </h5>
-      <h5>All Rights Reserved</h5>
+      <h5> All Rights Reserved</h5>
     </Wrapper>
   );
 };
