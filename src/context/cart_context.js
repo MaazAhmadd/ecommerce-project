5b99219376e1e78026777793be804c8aae5b9e96
Maaz <maaz.ahmad213@gmail.com>
2021-11-13 17:32:02 +0500

    toogle cart amount

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -37,7 +37,9 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: REMOVE_CART_ITEM, payload: id });
   };
   // toggleAmount
-  const toggleAmount = () => {};
+  const toggleAmount = (id, value) => {
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+  };
   // clearCart
   const clearCart = () => {
     dispatch({ type: CLEAR_CART });
