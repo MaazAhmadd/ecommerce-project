89985a54ac3d81dff1db2a9fffec688eb175c200
Maaz <maaz.ahmad213@gmail.com>
2021-11-14 02:13:54 +0500

    user context setup

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// dev-8s1vp0a8.us.auth0.com
+// t2oIMCLXmlcM67yQ6KoKFdm2Mnm79yaO
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-8s1vp0a8.us.auth0.com"
+    clientId="t2oIMCLXmlcM67yQ6KoKFdm2Mnm79yaO"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
